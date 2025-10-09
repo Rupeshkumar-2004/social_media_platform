8da62794fc0a36068b98775225eb6bdaf1754781
Rupesh Kumar <rockingdeepika123@gmail.com>
2025-10-09 23:04:39 +0530

    fix: updated some file

--- TASK 1/user.c
+++ TASK 1/user.c
@@ -2,10 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<stdbool.h>
-#include "platform.h"
-#include "post.h"
-#include "comment.h"
-#include "reply.h"
+
 #include "users.h"
 
 struct User* createUser(const char* username) {
@@ -34,4 +31,44 @@ struct User* findUser(struct User* head, const char* username) {
         temp = temp->next;
     }
     return NULL;
+}
+
+void displayUsers(struct User* head) {
+    struct User* temp = head;
+    while (temp) {
+        printf("Username: %s\n", temp->username);
+        temp = temp->next;
+    }
+}
+
+bool addUser(struct User** head, const char* username) {
+    if (findUser(*head, username)) {
+        return false; // User already exists
+    }
+    struct User* newUser = createUser(username);
+    if (!newUser) {
+        return false; // Memory allocation failed
+    }
+    newUser->next = *head;
+    *head = newUser;
+    return true;
+}
+
+bool deleteUserByUsername(struct User** head, const char* username) {
+    struct User* temp = *head;
+    struct User* prev = NULL;
+    while (temp) {
+        if (strcmp(temp->username, username) == 0) {
+            if (prev) {
+                prev->next = temp->next;
+            } else {
+                *head = temp->next;
+            }
+            deleteUser(temp);
+            return true;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return false; // User not found
 }
--- TASK 1/users.h
+++ TASK 1/users.h
@@ -10,6 +10,7 @@ struct User* createUser(const char* username);
 void deleteUser(struct User* user);
 struct User* findUser(struct User* head, const char* username);
 void displayUsers(struct User* head);
-
+bool addUser(struct User** head, const char* username);
+bool deleteUserByUsername(struct User** head, const char* username);
 
 #endif
